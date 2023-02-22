42350490a476f0dcec81253797049e95cac9c31e
StanimalTheMan <sjc591@nyu.edu>
2023-02-22 11:55:48 -0500

    Get room data using a custom hook useRoom based on room url

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,6 +1,14 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 import "./Chat.css";
+import useRoom from "../hooks/useRoom";
 
-export default function Chat() {
+export default function Chat({ user, page }) {
+  const { roomId } = useParams();
+
+  const room = useRoom(roomId, user.uid);
+
+  console.log({ room });
+
   return <div className="chat">Chat</div>;
 }

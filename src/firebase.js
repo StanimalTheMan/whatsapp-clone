a0772e17c9303a7172491120698b79ddff750b03
StanimalTheMan <sjc591@nyu.edu>
2023-02-23 13:01:10 -0500

    use env vars

---
+++ src/firebase.js
@@ -0,0 +1,40 @@
+// Import the functions you need from the SDKs you need
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage";
+import "firebase/database";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebaseApp.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+const storage = firebase.storage().ref("images");
+const audioStorage = firebase.storage().ref("audios");
+const createTimestamp = firebase.firestore.FieldValue.serverTimestamp;
+const serverTimestamp = firebase.database.ServerValue.TIMESTAMP;
+
+export {
+  db,
+  auth,
+  provider,
+  storage,
+  audioStorage,
+  createTimestamp,
+  serverTimestamp,
+};
